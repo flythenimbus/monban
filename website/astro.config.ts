8e3149b4de87fef13d7335b43e359571f405ec1e
flythenimbus <2448254+flythenimbus@users.noreply.github.com>
2026-04-03 20:24:05 -0600

    set up cloudflare pages

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -1,7 +1,9 @@
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	adapter: cloudflare(),
 	vite: {
 		plugins: [tailwindcss()],
 	},
